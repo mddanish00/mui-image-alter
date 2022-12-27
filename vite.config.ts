b4f8ec4f8df83b4221965fe38229620d16190c35
Miracutor <miracleexecutors@gmail.com>
2022-12-28 00:22:44 +0800

    Add test suite for mui-image

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -37,4 +40,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: 'jest-setup.ts',
+	},
 });
