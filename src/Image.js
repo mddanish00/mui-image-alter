3439e5cc847818fe792826fe138807cd8f52bf71
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-09-28 15:31:05 +0800

    refactor: use SvgIcon instead createSvgIcon

--- src/Image.js
+++ src/Image.js
@@ -3,13 +3,12 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 
 import { styled } from '@mui/material/styles';
-import { createSvgIcon } from '@mui/material/utils';
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 import CircularProgress from '@mui/material/CircularProgress';
 
-const BrokenImageIcon = createSvgIcon(
-	<path d="M21 5v6.59l-2.29-2.3c-.39-.39-1.03-.39-1.42 0L14 12.59 10.71 9.3a.9959.9959 0 0 0-1.41 0L6 12.59 3 9.58V5c0-1.1.9-2 2-2h14c1.1 0 2 .9 2 2zm-3 6.42 3 3.01V19c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2v-6.58l2.29 2.29c.39.39 1.02.39 1.41 0l3.3-3.3 3.29 3.29c.39.39 1.02.39 1.41 0l3.3-3.28z" />,
-	'BrokenImageIcon'
-);
+const BrokenImageIcon = (props)=>
+	<SvgIcon {...props}><path d="M21 5v6.59l-2.29-2.3c-.39-.39-1.03-.39-1.42 0L14 12.59 10.71 9.3a.9959.9959 0 0 0-1.41 0L6 12.59 3 9.58V5c0-1.1.9-2 2-2h14c1.1 0 2 .9 2 2zm-3 6.42 3 3.01V19c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2v-6.58l2.29 2.29c.39.39 1.02.39 1.41 0l3.3-3.3 3.29 3.29c.39.39 1.02.39 1.41 0l3.3-3.28z" /></SvgIcon>
+
 
 const Img = styled('img')({
 	'@keyframes materialize': {
