ffb13beb8e2cd095dad783a1c66459c0a5e4f2ec
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-09-28 15:36:38 +0800

    refactor: refactor demo to work with Vite

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -95,7 +95,7 @@ const EASING = 'cubic-bezier(0.7, 0, 0.6, 1)';
 const FIT = 'cover';
 const BG_COLOR = 'inherit';
 
-export default function Demo() {
+function Demo() {
 	const [currentPhoto, setCurrentPhoto] = React.useState(DEFAULT_IMAGE);
 	const [showPhoto, setShowPhoto] = React.useState(true);
 
@@ -489,10 +489,13 @@ export default function Demo() {
 	);
 }
 
-render(
-	<ThemeProvider theme={theme}>
-		<CssBaseline />
-		<Demo />
-	</ThemeProvider>,
-	document.querySelector('#demo')
-);
+const DemoComponent = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Demo />
+		</ThemeProvider>
+	);
+};
+
+export default DemoComponent;
