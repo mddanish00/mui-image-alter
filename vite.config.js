26c461eaef2054a2a8c0db8ea80e0f51874e8e7b
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-09-28 15:35:52 +0800

    feat: add Vite and needed files and configs

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { buildPlugin } from 'vite-plugin-build';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    buildPlugin({
+      libBuild: {
+        buildOptions: {
+          rollupOptions: {
+            external: ['react', /@mui\/.*/],
+            output: {
+              globals: {
+                react: 'React',
+                '@mui/material/CircularProgress': 'MaterialUI.CircularProgress',
+                '@mui/material/SvgIcon': 'MaterialUI.SvgIcon',
+                '@mui/material/styles/styled': 'MaterialUI.styled',
+              },
+            },
+          },
+          outDir: 'umd',
+          sourcemap: 'hidden',
+          lib: {
+            entry: resolve(__dirname, 'src/index.js'),
+            name: 'MuiImage',
+            formats: ['umd'],
+            fileName: () => `mui-image.js`,
+          },
+        },
+      },
+    }),
+  ],
+});
