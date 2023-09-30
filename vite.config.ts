2a359878f6b08203a9ab478f92ca6824c058726a
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-09-30 11:21:16 +0800

    fix: correct build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,42 @@
 /// <reference types="vite/client" />
 
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { buildPlugin } from 'vite-plugin-build';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    buildPlugin({
-      libBuild: {
-        buildOptions: {
-          rollupOptions: {
-            external: ['react', /@mui\/.*/],
-            output: {
-              globals: {
-                react: 'React',
-                '@mui/material/CircularProgress': 'MaterialUI.CircularProgress',
-                '@mui/material/SvgIcon': 'MaterialUI.SvgIcon',
-                '@mui/material/styles/styled': 'MaterialUI.styled',
-              },
-            },
-          },
-          outDir: 'umd',
-          sourcemap: 'hidden',
-          lib: {
-            entry: resolve(__dirname, 'src/index.js'),
-            name: 'MuiImage',
-            formats: ['umd'],
-            fileName: () => `mui-image.js`,
-          },
-        },
-      },
-    }),
-  ],
+	publicDir: false,
+	plugins: [
+		react(),
+		buildPlugin({
+			fileBuild: {
+				emitDeclaration: true,
+			},
+			libBuild: {
+				buildOptions: {
+					rollupOptions: {
+						external: ['react', /@mui\/.*/],
+						output: {
+							globals: {
+								react: 'React',
+								'@mui/material/CircularProgress': 'MaterialUI.CircularProgress',
+								'@mui/material/SvgIcon': 'MaterialUI.SvgIcon',
+								'@mui/material/styles/styled': 'MaterialUI.styled',
+							},
+						},
+					},
+					outDir: 'umd',
+					sourcemap: 'hidden',
+					lib: {
+						entry: resolve(__dirname, 'src/index.ts'),
+						name: 'MuiImage',
+						formats: ['umd'],
+						fileName: () => `mui-image.js`,
+					},
+				},
+			},
+		}),
+	],
 });
