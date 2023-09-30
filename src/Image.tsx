a4c41cc37d5db8508c8aaef7e6e11397f4b3125a
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-09-30 10:41:31 +0800

    feat: add ForwardRef and component prop support

--- src/Image.tsx
+++ src/Image.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 import CircularProgress from '@mui/material/CircularProgress';
-import { ImageProps } from './Image.types';
+import { ImageProps, ImageTypeMap, MuiImage } from './Image.types';
 
 const BrokenImageIcon = (props: SvgIconProps) => (
 	<SvgIcon {...props}>
@@ -11,95 +11,100 @@ const BrokenImageIcon = (props: SvgIconProps) => (
 	</SvgIcon>
 );
 
-export default function Image(props: ImageProps<'img'>) {
-	const {
-		src,
-		alt = '',
-		height,
-		width,
-		position = 'relative',
-		fit = 'cover',
-		style,
-		className = '',
-		showLoading = false,
-		errorIcon = true,
-		shift = false,
-		distance = 100,
-		shiftDuration = undefined,
-		bgColor = 'inherit',
-		wrapperStyle,
-		iconWrapperStyle,
-		wrapperClassName = '',
-		iconWrapperClassName = '',
-		duration = 3000,
-		easing = 'cubic-bezier(0.7, 0, 0.6, 1)', // "heavy move" from https://sprawledoctopus.com/easing/,
-		onLoad: onLoadProp,
-		onError: onErrorProp,
-		...rest
-	} = props;
+const Image = React.forwardRef(
+	<BaseComponentType extends React.ElementType = ImageTypeMap['defaultComponent']>(
+		{
+			src,
+			alt = '',
+			position = 'relative',
+			fit = 'cover',
+			style,
+			className = '',
+			showLoading = false,
+			errorIcon = true,
+			shift = false,
+			distance = 100,
+			shiftDuration = undefined,
+			bgColor = 'inherit',
+			wrapperStyle,
+			iconWrapperStyle,
+			wrapperClassName = '',
+			iconWrapperClassName = '',
+			duration = 3000,
+			easing = 'cubic-bezier(0.7, 0, 0.6, 1)', // "heavy move" from https://sprawledoctopus.com/easing/,
+			onLoad: onLoadProp,
+			onError: onErrorProp,
+			sx,
+			component = 'img',
+			...rest
+		}: ImageProps<BaseComponentType>,
+		ref: React.ForwardedRef<any>,
+	) => {
+		const [loaded, setLoaded] = React.useState(false);
+		const [error, setError] = React.useState(false);
 
-	const [loaded, setLoaded] = React.useState(false);
-	const [error, setError] = React.useState(false);
+		const handleLoad = React.useCallback(() => {
+			setLoaded(true);
+			setError(false);
+			if (onLoadProp) onLoadProp();
+		}, [onLoadProp]);
 
-	const handleLoad = React.useCallback(() => {
-		setLoaded(true);
-		setError(false);
-		if (onLoadProp) onLoadProp();
-	}, [onLoadProp]);
+		const handleError = React.useCallback(() => {
+			setError(true);
+			setLoaded(false);
+			if (onErrorProp) onErrorProp();
+		}, [onErrorProp]);
 
-	const handleError = React.useCallback(() => {
-		setError(true);
-		setLoaded(false);
-		if (onErrorProp) onErrorProp();
-	}, [onErrorProp]);
+		const shiftStyles =
+			shift !== undefined && shift !== false && shift !== null
+				? {
+						[shift]: loaded ? 0 : distance,
+				  }
+				: {};
 
-	const shiftStyles =
-		shift !== undefined && shift !== false && shift !== null
-			? {
-					[shift]: loaded ? 0 : distance,
-			  }
-			: {};
+		const showErrorIcon = (typeof errorIcon !== 'boolean' && errorIcon) || (
+			<BrokenImageIcon style={{ fontSize: 56, color: '#bdbdbd' }} /> // MUI grey[400]
+		);
 
-	const showErrorIcon = (typeof errorIcon !== 'boolean' && errorIcon) || (
-		<BrokenImageIcon style={{ fontSize: 56, color: '#bdbdbd' }} /> // MUI grey[400]
-	);
+		const loadingIndicator = (typeof showLoading !== 'boolean' && showLoading) || (
+			<CircularProgress size={56} thickness={6} />
+		);
 
-	const loadingIndicator = (typeof showLoading !== 'boolean' && showLoading) || (
-		<CircularProgress size={56} thickness={6} />
-	);
-
-	return (
-		<MuiImageWrapper style={wrapperStyle} className={`mui-image-wrapper ${wrapperClassName}`}>
-			<MuiImageRoot
-				src={src}
-				alt={alt}
-				style={style}
-				className={`mui-image-img ${className}`}
-				onLoad={handleLoad}
-				onError={handleError}
-				position={position}
-				fit={fit}
-				shift={shift}
-				shiftDuration={shiftDuration}
-				shiftStyles={shiftStyles}
-				duration={duration}
-				easing={easing}
-				loaded={loaded}
-				{...rest}
-			/>
-			{(Boolean(showLoading) || Boolean(errorIcon)) && (
-				<MuiImageIconWrapper
-					style={iconWrapperStyle}
-					className={`mui-image-iconWrapper ${iconWrapperClassName}`}
+		return (
+			<MuiImageWrapper style={wrapperStyle} className={`mui-image-wrapper ${wrapperClassName}`}>
+				<MuiImageRoot
+					ref={ref}
+					as={component}
+					src={src}
+					alt={alt}
+					style={style}
+					className={`mui-image-img ${className}`}
+					onLoad={handleLoad}
+					onError={handleError}
+					position={position}
+					fit={fit}
+					shift={shift}
+					shiftDuration={shiftDuration}
+					shiftStyles={shiftStyles}
+					duration={duration}
+					easing={easing}
 					loaded={loaded}
-				>
-					{Boolean(errorIcon) && error && showErrorIcon}
-					{Boolean(showLoading) && !error && !loaded && loadingIndicator}
-				</MuiImageIconWrapper>
-			)}
-		</MuiImageWrapper>
-	);
-}
+					{...rest}
+				/>
+				{(Boolean(showLoading) || Boolean(errorIcon)) && (
+					<MuiImageIconWrapper
+						style={iconWrapperStyle}
+						className={`mui-image-iconWrapper ${iconWrapperClassName}`}
+						loaded={loaded}
+					>
+						{Boolean(errorIcon) && error && showErrorIcon}
+						{Boolean(showLoading) && !error && !loaded && loadingIndicator}
+					</MuiImageIconWrapper>
+				)}
+			</MuiImageWrapper>
+		);
+	},
+) as MuiImage<ImageTypeMap>;
 
 // Utility functions
 const checkProps = (value: string, arr: string[]) => !arr.includes(value);
@@ -183,3 +188,5 @@ const MuiImageIconWrapper = React.memo(
 		opacity: props.loaded ? 0 : 1,
 	})),
 );
+
+export default Image;
