be85ec0d57eb7da80ad4d0403aa02ca267f05787
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-10-14 17:39:10 +0800

    feat: adjust imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import Image from './Image';
+export { default } from './Image';
+export * from './Image';
 
-export default Image;
-
-export { default as Image } from './Image';
+export * from './Image.types';
