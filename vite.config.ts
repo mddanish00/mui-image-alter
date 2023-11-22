afdb76ca8479ca5dc2cf6df5856fe4630c35b34b
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2023-11-22 16:20:25 +0800

    build: build library using tsup instead of Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,13 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { buildPlugin } from 'vite-plugin-build';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	publicDir: false,
-	plugins: [
-		react(),
-		buildPlugin({
-			fileBuild: {
-				emitDeclaration: true,
-				rollupOptionsOutput: { interop: 'auto' },
-			},
-			libBuild: {
-				buildOptions: {
-					rollupOptions: {
-						external: ['react', /@mui\/.*/, 'use-resize-observer'],
-						output: {
-							globals: {
-								react: 'React',
-								'@mui/material/CircularProgress': 'MaterialUI.CircularProgress',
-								'@mui/material/SvgIcon': 'MaterialUI.SvgIcon',
-								'@mui/material/styles/styled': 'MaterialUI.styled',
-								'use-resize-observer': 'UseResizeObserver',
-							},
-						},
-					},
-					outDir: 'umd',
-					sourcemap: 'hidden',
-					lib: {
-						entry: resolve(__dirname, 'src/index.ts'),
-						name: 'MuiImage',
-						formats: ['umd'],
-						fileName: () => `mui-image.js`,
-					},
-				},
-			},
-		}),
-	],
+	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
