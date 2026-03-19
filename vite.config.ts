b8b853bb24fbfda875d0fafcecb4adc568155f93
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2026-03-20 02:43:41 +0800

    feat: update to vite v8
    feat: use unplugin-dts instead of old plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
+import dts from 'unplugin-dts/vite';
 import { resolve } from 'path';
 import fs from 'fs';
 
@@ -17,19 +17,21 @@ export default defineConfig({
 			fileName: 'index',
 			formats: ['es', 'cjs'],
 		},
-		rollupOptions: {
+		rolldownOptions: {
 			external: ['react', /@mui\/.*/, 'use-resize-observer', 'react/jsx-runtime', 'clsx'],
 			output: {
-				interop: 'auto',
-				inlineDynamicImports: true,
+				codeSplitting: false,
 				banner: '"use client";',
+			},
+			checks: {
+				pluginTimings: false,
 			},
 		},
 	},
 	plugins: [
 		react(),
 		dts({
-			rollupTypes: true,
+			bundleTypes: true,
 			afterBuild: () => {
 				fs.copyFileSync('dist/index.d.cts', 'dist/index.d.ts');
 			},
